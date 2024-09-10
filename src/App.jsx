de882d3a00b526a70f29beb4edb986566a4a2afc
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-09-10 17:30:49 +0530

    Git comit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
+import Values from "./components/Values/Values.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import GetStarted from "./components/GetStarted/GetStarted.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
+      <Values />
+      <Contact />
+      <GetStarted />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
