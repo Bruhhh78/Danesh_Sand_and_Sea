ed6f36371133636c5414d8a8ad21a17e09253609
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-09-20 17:50:32 +0530

    Git Commit Initialising

---
+++ server/controllers/userController.js
@@ -0,0 +1,10 @@
+import asyncHandler from "express-async-handler";
+
+import { prisma } from "../config/prismaConfig.js";
+
+export const createUser = asyncHandler(async (req, res) => {
+  console.log("Creating User...");
+
+  let { email } = req.body;
+  console.log(email);
+});
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express"; // Express framework for building the server
 import dotenv from "dotenv"; // dotenv module to load environment variables from a .env file
 import cookieParser from "cookie-parser"; // Middleware to parse cookies
 import cors from "cors"; // Middleware to handle Cross-Origin Resource Sharing (CORS)
+import { userRoute } from "./routes/userRoute.js";
 
 dotenv.config(); // Load environment variables
 
@@ -19,7 +20,12 @@ app.get("/", function (req, res) {
 });
 
 
+app.use('/api/user',userRoute)
+
+
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/`);
 });
+
+
