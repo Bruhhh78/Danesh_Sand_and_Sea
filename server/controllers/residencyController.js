ce8c91f1e47c7cf7b6aee18f52c226c18955592f
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-09-21 16:25:31 +0530

    Git Commit Backend Initialising

--- server/controllers/residencyController.js
+++ server/controllers/residencyController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import { prisma } from '../config/prismaConfig.js'; // Adjust the path as needed
+import { prisma } from "../config/prismaConfig.js"; // Adjust the path as needed
 
 // Controller function to create a residency
 export const createResidency = asyncHandler(async (req, res) => {
@@ -29,13 +29,40 @@ export const createResidency = asyncHandler(async (req, res) => {
         owner: { connect: { email: userEmail } },
       },
     });
-    res.status(201).send({ message: "Residency created successfully", residency });
+    res
+      .status(201)
+      .send({ message: "Residency created successfully", residency });
   } catch (err) {
     console.error(err);
     if (err.code === "P2002") {
-      res.status(400).send({ message: "A residency with this address already exists for this user." });
+      res.status(400).send({
+        message: "A residency with this address already exists for this user.",
+      });
     } else {
       res.status(500).send({ message: err.message });
     }
+  }
+});
+
+export const getAllResidencies = asyncHandler(async (req, res) => {
+  const residencies = await prisma.residency.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  res.send(residencies);
+});
+
+//function to get a specific document/residency
+export const getResidency = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const residency = await prisma.residency.findUnique({
+      where: { id },
+    });
+    res.send(residency);
+  } catch (err) {
+    throw new Error(err.message);
   }
 });
