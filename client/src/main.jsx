4c501c9c5651f2fdac1293ff4492bda294316f5d
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-10-01 17:11:33 +0530

    Git Commit Initializing Frontend Authorization

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')).render(
+// Access environment variables
+// console.log("Auth0 Domain:", import.meta.env.VITE_AUTH0_DOMAIN);
+// console.log("Auth0 Client ID:", import.meta.env.VITE_AUTH0_CLIENT_ID);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+      }}
+      audience={import.meta.env.VITE_AUTH0_AUDIENCE}
+      scope={import.meta.env.VITE_AUTH0_SCOPE}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
